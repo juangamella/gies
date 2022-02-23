281593e1addbce1c8ab5137968066034998586ce
orugak <olgako@student.ethz.ch>
2022-02-23 15:30:51 +0100

    greedy algo sketch

---
+++ ges/test/greedy_score.py
@@ -0,0 +1,54 @@
+import numpy as np
+import ges
+import networkx as nx
+import sempler
+from sempler.generators import dag_avg_deg
+import itertools
+from ges.scores.exp_gauss_int_l0_pen import ExpGaussIntL0Pen
+np.set_printoptions(suppress=True)
+k = 4
+num_tot = 100
+num_true = 0
+for m in range(num_tot):
+    while True:
+        A = dag_avg_deg(k, 2)
+        nxA = nx.from_numpy_array(A, create_using=nx.MultiDiGraph())
+        nxA_undir = nx.DiGraph.to_undirected(nxA)
+        if nx.is_directed_acyclic_graph(nxA) and nx.is_connected(nxA_undir):
+            break
+    W = A * np.random.uniform(1, 2, size=A.shape)
+    scm = sempler.LGANM(W, (0, 15), (0, 0.2))
+    n = 200000
+    obs_data = scm.sample(n=n)
+    obs_data2 = scm.sample(n=n)
+    interv_data = scm.sample(n=n, do_interventions={1: (2, 10)})
+    # interv_data2 = scm.sample(n=n, do_interventions={2: (2, 10)})
+    data = [obs_data, interv_data]
+    true_interv = [[], [1]]
+    true_pdag = ges.exp_fit_bic(data, true_interv)[0]
+    pdag_A = ges.utils.replace_unprotected(A, true_interv)
+    print(pdag_A)
+    print(true_pdag)
+    if not np.all(pdag_A == true_pdag):
+        num_tot -= 1
+        continue
+    A0 = np.zeros_like(A)
+
+    max_interv = [[], []]
+    max_pdag = ges.exp_fit_bic(data, max_interv)[0]
+    max_score = ExpGaussIntL0Pen(data, max_interv).full_score(ges.utils.pdag_to_dag(max_pdag))
+
+    for i in range(k):
+        interv = [[], [i]]
+        pdag = ges.exp_fit_bic(data, interv)[0]
+        score = ExpGaussIntL0Pen(data, interv).full_score(ges.utils.pdag_to_dag(pdag))
+        if score > max_score:
+            max_interv = interv
+            max_pdag = pdag
+            max_score = score
+
+    if max_interv == true_interv:
+        num_true += 1
+    print(m)
+print(num_true/num_tot)
+
