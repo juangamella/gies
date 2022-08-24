a2fd031d01b23c39d4e2ea952146d6018f43fbad
Juan Gamella Martin <juan.gamella@stat.math.ethz.ch>
2022-08-24 19:03:07 +0200

    Updated examples

--- docs/gies_fit_bic_example.py
+++ docs/gies_fit_bic_example.py
@@ -13,7 +13,7 @@ A = np.array(
     ]
 )
 W = A * np.random.uniform(1, 2, A.shape)  # sample weights
-scm = sempler.LGANM(W, (0, 0), (1, 2))
+scm = sempler.LGANM(W, (1, 2), (1, 2))
 data = [scm.sample(n=5000), scm.sample(n=5000, do_interventions={2: (0, 5)})]
 
 # Run GIES with the gaussian BIC score
